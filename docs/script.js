47e0344bce4ff6151a4442b29abf0a12d5af4718
David Megginson <david.megginson@gmail.com>
2020-02-01 14:53:51 -0500

    Added wind problems.

--- docs/script.js
+++ docs/script.js
@@ -1,5 +1,8 @@
 var e6b = {
-    questions: {},
+    questions: {
+        calc: {},
+        wind: {}
+    },
     nodes: {}
 };
 
@@ -16,7 +19,83 @@ e6b.time = function(minutes) {
     return h + ":" + m;
 };
 
-e6b.questions.volumeUnits = function () {
+e6b.wind_params = function () {
+    var params = {};
+    params.track = e6b.rand(0, 360);
+    params.tas = e6b.rand(60, 250);
+    params.wind_dir = e6b.rand(0, 36) * 10;
+    params.wind_speed = e6b.rand(5, 40);
+
+    var delta = params.wind_dir - params.track;
+
+    var cos = Math.cos(delta * (Math.PI / 180.0));
+    var sin = Math.sin(delta * (Math.PI / 180.0));
+    params.headwind = Math.round(params.wind_speed * cos);
+    params.crosswind = Math.round(params.wind_speed * sin);
+
+    var cos2 = params.tas / Math.sqrt(params.tas * params.tas + params.crosswind * params.crosswind);
+    var effective_speed = Math.round(params.tas * cos2);
+    params.gs = effective_speed - params.headwind;
+
+    var delta2 = Math.round(Math.acos(cos2) * (180 / Math.PI)) * (Math.abs(params.wind_dir - params.track) < 180 ? 1 : -1);
+    params.heading = params.track + delta2;
+
+    return params;
+};
+
+
+////////////////////////////////////////////////////////////////////////
+// Questions for the wind side
+////////////////////////////////////////////////////////////////////////
+
+e6b.questions.wind.wind = function () {
+    var params = e6b.wind_params();
+    return [
+        "Calculate actual winds: track " + params.track + "°, heading " + params.heading + "°, " + params.tas + " kt TAS, " + params.gs + " kt GS.",
+        "Winds are from " + params.wind_dir + "° @ " + params.wind_speed + " kt."
+    ];
+};
+
+e6b.questions.wind.heading = function () {
+    var params = e6b.wind_params();
+    return [
+        "Calculate heading: desired course " + params.track + "°, airspeed " + params.tas + " ktas, winds " + params.wind_dir + "@" + params.wind_speed + " kt.",
+        "Required heading is " + params.heading + "°."
+    ];
+};
+
+e6b.questions.wind.groundspeed = function () {
+    var params = e6b.wind_params();
+    return [
+        "Calculate groundspeed: desired course " + params.track + "°, airspeed " + params.tas + " ktas, winds " + params.wind_dir + "@" + params.wind_speed + " kt.",
+        "Groundspeed is " + params.gs + " kt."
+    ];
+};
+
+e6b.questions.wind.headwind = function () {
+    var params = e6b.wind_params();
+    return [
+        "Calculate headwind/tailwind: desired course " + params.track + "°, airspeed " + params.tas + " ktas, winds " + params.wind_dir + "@" + params.wind_speed + " kt.",
+        "There is a " + Math.abs(params.headwind) + (params.headwind < 0 ? "  kt tailwind component." : " kt headwind component.")
+    ];
+};
+
+e6b.questions.wind.crosswind = function () {
+    var params = e6b.wind_params();
+    return [
+        "Calculate crosswind: " + params.track + "°, airspeed " + params.tas + " ktas, winds " + params.wind_dir + "@" + params.wind_speed + " kt.",
+        "The crosswind is " + Math.abs(params.headwind) + " kt from the " + (params.crosswind < 0 ? "left." : "right.")
+    ];
+};
+
+
+
+
+////////////////////////////////////////////////////////////////////////
+// Questions for the calculator side.
+////////////////////////////////////////////////////////////////////////
+
+e6b.questions.calc.volumeUnits = function () {
     var gallons = e6b.rand(3, 150);
     var litres = Math.floor(gallons * 3.78541);
     switch (e6b.rand(0, 2)) {
@@ -34,7 +113,7 @@ e6b.questions.volumeUnits = function () {
 };
 
 
-e6b.questions.distanceUnits = function () {
+e6b.questions.calc.distanceUnits = function () {
     var units = ['nautical miles', 'statue miles', 'kilometers'];
     var values = [];
     values [0] = e6b.rand(10, 300);
@@ -54,7 +133,7 @@ e6b.questions.distanceUnits = function () {
 };
 
 
-e6b.questions.fuelTimeDistance = function () {
+e6b.questions.calc.fuelTimeDistance = function () {
     var fuel_burn = e6b.rand(50, 300) / 10.0;
     var time = e6b.rand(5, 180);
     var gallons = Math.round(fuel_burn / 6.0 * time) / 10.0;
@@ -79,7 +158,7 @@ e6b.questions.fuelTimeDistance = function () {
 };
 
 
-e6b.questions.speedTimeDistance = function () {
+e6b.questions.calc.speedTimeDistance = function () {
     var speed = e6b.rand(60, 300);
     var time = e6b.rand(5, 180);
     var distance = Math.round(speed / 60.0 * time);
@@ -103,9 +182,17 @@ e6b.questions.speedTimeDistance = function () {
     }
 };
 
+e6b.rand_item = function (obj) {
+    var keys = Object.keys(obj);
+    return obj[keys[keys.length * Math.random() << 0]];
+};
+
 e6b.question = function () {
-    var keys = Object.keys(e6b.questions);
-    var qfun = e6b.questions[keys[keys.length * Math.random() << 0]];
+    if (e6b.type == 'wind') {
+        var qfun = e6b.rand_item(e6b.questions.wind);
+    } else {
+        var qfun = e6b.rand_item(e6b.questions.calc);
+    }
     var info = qfun();
     e6b.nodes.answer.hidden = true;
     e6b.nodes.question.textContent = info[0];
